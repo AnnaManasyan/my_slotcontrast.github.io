d11e5ecb8f8a62adc854b744c05f7ae50f5368d4
anna <annamanasyan4@gmail.com>
2024-12-04 15:57:14 +0100

    final

--- static/js/index.js
+++ static/js/index.js
@@ -5,17 +5,18 @@ $(document).ready(function() {
     // Check for click events on the navbar burger icon
 
     var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 5000,
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        loop: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 10000, // Set this to a higher value (e.g., 10000 for 10 seconds)
     }
 
-		// Initialize all div with carousel class
+    // Initialize all div with carousel class
     var carousels = bulmaCarousel.attach('.carousel', options);
-	
+    
     bulmaSlider.attach();
 
-})
+});
+
